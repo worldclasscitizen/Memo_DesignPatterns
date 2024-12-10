ed91c46111e031cfa0c1c7536013c3dc41385ead
Jinwoo Choi <jk62362@mensakorea.org>
2024-12-10 18:29:05 +0900

    feat: Proxy Pattern 예제 코드 추가

---
+++ src/ProxyPattern/IService.java
@@ -0,0 +1,5 @@
+package ProxyPattern;
+
+public interface IService {
+    String runSomething();
+}
---
+++ src/ProxyPattern/Service.java
@@ -0,0 +1,7 @@
+package ProxyPattern;
+
+public class Service implements IService {
+    public String runSomething() {
+        return "서비스 짱 !";
+    }
+}
