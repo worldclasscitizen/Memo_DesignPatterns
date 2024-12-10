716af15791c8f326d619e75d6587fc26ab4c480d
Jinwoo Choi <jk62362@mensakorea.org>
2024-12-11 01:47:39 +0900

    feat: Strategy Pattern 예제 코드 추가

---
+++ src/StrategyPattern/StrategyBow.java
@@ -0,0 +1,8 @@
+package StrategyPattern;
+
+public class StrategyBow implements Strategy {
+    @Override
+    public void runStrategy() {
+        System.out.println("슝.. 쐐액.. 쉑, 최종 병기");
+    }
+}
---
+++ src/StrategyPattern/StrategySword.java
@@ -0,0 +1,8 @@
+package StrategyPattern;
+
+public class StrategySword implements Strategy {
+    @Override
+    public void runStrategy() {
+        System.out.println("챙.. 채쟁챙 챙챙");
+    }
+}
